4a95ed0b27e11bd39713a14c4e03383d1f97b378
Axelynho <honplus45@mail.ru>
2023-01-12 19:36:42 +0400

    controller.js / pause

---
+++ src/controller.js
@@ -0,0 +1,79 @@
+export default class Contoller {
+    constructor(game, view) {
+        this.game = game;
+        this.view = view;
+        this.intervalId = null;
+        this.isPlaying = false;
+
+        document.addEventListener('keydown',this.handleKeyDown.bind(this));
+
+        this.view.renderStartScreen();
+    }
+
+    update() {
+        this.game.movePieceDown();
+        this.updateView();
+    }
+
+    play() {
+        this.isPlaying = true;
+        this.startTimer();
+        this.updateView();
+    }
+
+    pause() {
+        this.isPlaying = false;
+        this.stopTimer();
+        this.updateView();
+    }
+
+    updateView() {
+        if (!this.isPlaying) {
+            this.view.renderPauseScreen();
+        } else {
+            this.view.renderMainScreen(this.game.getState());
+        }
+    }
+
+    startTimer() {
+        if (!this.intervalId) {this.intervalId = setInterval(() => {
+            this.update();
+            }, 1000);
+        }
+    }
+
+    stopTimer() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    handleKeyDown(event) {
+        switch (event.keyCode) {
+            case 13: // ENTER
+            if(this.isPlaying) {
+                this.pause();
+            } else {
+                this.play();
+            }
+            break;
+            case 37: //left
+                game.movePieceLeft();
+                view.renderMainScreen(this.game.getState());
+                break;
+            case 38: // up
+                game.rotatePiece();
+                view.renderMainScreen(this.game.getState());
+                break;
+            case 39: // right
+            game.movePieceRight();
+            view.renderMainScreen(this.game.getState());
+                break;
+            case 40: // down
+            game.movePieceDown();
+            view.renderMainScreen(this.game.getState());
+                break;
+            }
+        }
+    }
